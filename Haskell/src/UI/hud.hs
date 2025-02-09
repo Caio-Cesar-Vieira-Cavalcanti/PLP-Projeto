f18f7dae55f7d0b5962f5065c4d7c4d6f6762f1c
Valdemar Carvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2025-02-09 15:05:22 -0300

    saveJogoScreen && mercadoScreen

--- Haskell/src/UI/hud.hs
+++ Haskell/src/UI/hud.hs
@@ -1,7 +1,7 @@
---Tela Principal da HUD abaixo
+-- Tela Principal da HUD 
 
-main_screen :: IO()
-main_screen = do
+mainScreen :: IO()
+mainScreen = do
     -- let jogador = "Neymar"
     -- let primeira_linha = "          Jogador: " ++ jogador ++ "          Oponente: Bot"
     -- putStrLn primeira_linha
@@ -18,20 +18,11 @@ main_screen = do
     let minasOuroAtingidas = 2
     let minasOuroTotais = 5
     putStrLn ("Minas de ouro: " ++ show minasOuroAtingidas ++ "/" ++ show minasOuroTotais)
+    
     putStrLn ""
 
-    putStrLn "Inventário:"
-    let bombasPequenas = 3
-    let bombasMedias = 1
-    let bombasGrandes = 0
-    putStrLn ("Bombas pequenas: " ++ show bombasPequenas ++ " | Bombas médias: " ++ show bombasMedias ++ " | Bombas grandes: " ++ show bombasGrandes)
-
-    let drones = 2
-    putStrLn ("Drone visualizador de áreas: " ++ show drones)
-
-    putStrLn ""
-    let moedas = 25
-    putStrLn ("Moedas: " ++ show moedas)
+    mostraInventario
+    
     putStrLn ""
 
     putStrLn "Atalhos: "
@@ -43,9 +34,72 @@ main_screen = do
     putStrLn "'m' -> Acesso ao mercado"
     putStrLn "'s' -> Salvar o jogo no estado atual"
     putStrLn "'quit' -> Sair do jogo sem salvar"
+    
+    putStrLn ""
 
+    putStr "> Digite a opção ou coordenadas:"
 
---Tela salvar jogo abaixo
+-- Tela Salvar Jogo
 
+saveJogoScreen :: IO ()
+saveJogoScreen = do
+    putStrLn "Escolha um slot para salvar:"
+    
+    putStrLn ""
 
---Tela mercado abaixo
+    putStrLn "[1] Slot 1 - Vazio"
+    putStrLn "{nome jog.} - Jogo Salvo em {data}"
+    putStrLn "{nome jog.} - Jogo Salvo em {data}"
+    
+    putStrLn ""
+
+    putStr "> Digite o slot ou 'v' para voltar: "
+
+-- Tela Mercado 
+
+mercadoScreen :: IO ()
+mercadoScreen = do
+
+    putStrLn "  __  __                             _        "
+    putStrLn " |  \x5C/  |  ___  _ __  ___  __ _   __| |  ___  "
+    putStrLn " | |\x5C/| | / _ \x5C| '__|/ __|/ _` | / _` | / _ \x5C "
+    putStrLn " | |  | ||  __/| |  | (__| (_| || (_| || (_) |"
+    putStrLn " |_|  |_| \x5C___||_|   \x5C___|\x5C__,_| \x5C__,_| \x5C___/ "
+
+    putStrLn ""
+
+    mostraInventario
+    
+    putStrLn ""
+
+    putStrLn "Atalhos:"
+    
+    putStrLn ""
+
+    putStrLn "'1' -> Comprar bombas pequenas             ({preço})"
+    putStrLn "'2' -> Comprar bombas médias               ({preço})"
+    putStrLn "'3' -> Comprar bombas grandes              ({preço})"
+    putStrLn "'4' -> Comprar drone visualizador de áreas ({preço})"
+
+    putStrLn ""
+
+    putStr "> Digite o item ou 'v' para voltar:"
+
+-- Funções utilitárias
+
+mostraInventario :: IO ()
+mostraInventario = do
+    putStrLn "Inventário:"
+    
+    let bombasPequenas = 3
+    let bombasMedias = 1
+    let bombasGrandes = 0
+    putStrLn ("Bombas pequenas: " ++ show bombasPequenas ++ " | Bombas médias: " ++ show bombasMedias ++ " | Bombas grandes: " ++ show bombasGrandes)
+
+    let drones = 2
+    putStrLn ("Drone visualizador de áreas: " ++ show drones)
+
+    putStrLn ""
+    
+    let moedas = 25
+    putStrLn ("Moedas: " ++ show moedas)
