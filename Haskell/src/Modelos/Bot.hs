5af15d82fc517ddde79a777cf39b302c41fe9870
italianoW <wendelitaliano2@gmail.com>
2025-02-25 23:04:58 -0300

    Implementação: Bot.hs (requer alterações)

--- Haskell/src/Modelos/Bot.hs
+++ Haskell/src/Modelos/Bot.hs
@@ -1,3 +1,22 @@
-module Modelos.Bot where
+module Modelos.Bot (Bot(..), BotClass(..)) where
 
--- Lógica do objeto Bot (semelhante ao jogador)
+import Modelos.Tabuleiro ( Tabela )
+import Modelos.Coordenada (Coordenada)
+import System.Random (randomRIO)
+--Necessário adicionar random
+
+class BotClass b where
+    getTabela :: b -> Tabela
+    getJogada :: b -> (Int,Int)
+
+data Bot = Bot
+    { 
+     tabela :: Tabela
+    } deriving Show
+
+instance BotClass Bot where
+
+    getTabela = tabela
+    --getJogada = {
+        -- Transforma coordenada randômica (dentre as ainda não acertadas) em acertada e retorna a tupla (x,y)
+    --}
