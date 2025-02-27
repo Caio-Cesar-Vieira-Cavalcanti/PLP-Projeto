4a7211fd8bfd417c1f451edfe3449556d4d4bf32
jpedro8azevedo <joao.pedro.azevedo.nascimento@ccc.ufcg.edu.br>
2025-02-26 22:46:31 -0300

    ajuste em atirouNaCoordenada e setElemEspecial feito

--- Haskell/src/Modelos/Tabuleiro.hs
+++ Haskell/src/Modelos/Tabuleiro.hs
@@ -1,4 +1,4 @@
-module Modelos.Tabuleiro (Tabela, geraTabela, geraTabelaStr, atirouNaCoordenada) where
+module Modelos.Tabuleiro (Tabela, geraTabela, geraTabelaStr, atirouNaCoordenada, setElemEspecial) where
 
 import Modelos.Coordenada
 
@@ -16,12 +16,8 @@ colocaLetrasNumeros :: [[String]] -> [[String]]
 colocaLetrasNumeros listaSemNumeros = ["  ", "A ", "B ", "C ", "D ", "E ", "F ", "G ", "H ", "I ", "J ", "K ", "L "] : [ (show i ++ " ") : x | (x, i) <- zip listaSemNumeros [1..12] ]
 
 
-atirouNaCoordenada :: Tabela -> Int -> Int -> Coordenada -> Tabela
-atirouNaCoordenada tabela c l coord = [[if i == l && j == c then Coordenada (getMascara coord) (getElemEspecial coord) True else x | (j, x) <- zip [0..] linha] | (i, linha) <- zip [0..] tabela]
+atirouNaCoordenada :: Tabela -> Int -> Int -> Tabela
+atirouNaCoordenada tabela c l = [[if i == l && j == c then Coordenada (getMascara x) (getElemEspecial x) True else x | (j, x) <- zip [0..] linha] | (i, linha) <- zip [0..] tabela]
 
-
--- checaSeFoiAcertado :: Coordenada -> String
--- checaSeFoiAcertado coord =
---     if acertou coord
---         then representacao coord
---         else "X"
+setElemEspecial :: Tabela -> Int -> Int -> Char -> Tabela
+setElemEspecial tabela c l novoElemEspecial = [[if i == l && j == c then Coordenada (getMascara x) novoElemEspecial False else x | (j, x) <- zip [0..] linha] | (i, linha) <- zip [0..] tabela]
