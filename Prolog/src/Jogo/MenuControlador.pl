e53050bc6a790f983b0d63d4fc7934031cc8f9d9
valdemarvictorleitecarvalho <valdemar.victor.leite.carvalho@ccc.ufcg.edu.br>
2025-04-05 13:10:48 -0300

    Chamadas Parciais Jogo no MenuControlador

--- Prolog/src/Jogo/MenuControlador.pl
+++ Prolog/src/Jogo/MenuControlador.pl
@@ -4,7 +4,11 @@
 :- use_module('../UI/Menu.pl').
 :- use_module('../UI/UtilsUI.pl').
 
-% AUSENTE A IMPORTAÇÃO DOS MODELOS E DEMAIS ARQUIVOS NECESSÁRIOS!
+:- use_module('../Modelos/Jogo.pl').
+:- use_module('../Modelos/Jogador.pl').
+:- use_module('../Modelos/Mercado.pl').
+
+% AUSENTE A IMPORTAÇÃO DE TODOS OS MODELOS E DEMAIS ARQUIVOS NECESSÁRIOS!
 
 % Limpar a tela
 clear_screen :- 
@@ -79,10 +83,49 @@ processarSubOpcao :-
 
 % =========================================================================JOGO================================================================================
 
+iniciarJogo(NomeJogador) :-
+    inicializarJogo(NomeJogador, Jogo),
+    loopJogo(Jogo).
 
-    
+carregarJogo(Numero) :-
+    string_concat('../BD/save', Numero, Caminho),
+    carregarSave(Caminho, Resultado),
+    loopJogo(Resultado).
+carregarJogo(_) :-
+    writeln('Erro: Não foi possível carregar o jogo.').
 
+% Falta a condição de vitória e derrota
+loopJogo(Jogo) :- 
+    clear_screen,
+    getJogador(Jogo, Jogador),
+    getNome(Jogador, Nome), 
+    mainScreen(Nome).
 
+processarOpcaoLoop("M", Jogo) :- 
+    clear_screen,
+    mercadoScreen(Jogo),
+    read_line_to_string(user_input, Item),
+    ( member(Item, ["1", "2", "3"]) ->
+        getJogador(Jogo, Jogador),
+        getMercado(Jogo, Mercado),
+        comprarItem(Jogador, Mercado, Item, NovoJogador),
+        setJogador(Jogo, NovoJogador, NovoJogo),
+        loopJogo(NovoJogo)
+    ; processarSubOpcaoLoop(Jogo, Item)
+    ).
 
+processarOpcaoLoop(_, Jogo) :-
+    opcaoInvalida,
+    read_line_to_string(user_input, Opcao),
+    processarOpcaoLoop(Opcao, Jogo).
 
+% Funções auxiliares JOGO
 
+processarSubOpcaoLoop(Jogo, Opcao) :-
+    member(Opcao, ["V", "v"]),
+    loopJogo(Jogo).
+
+processarSubOpcaoLoop(Jogo, _) :-
+    opcaoInvalidaVoltar,
+    read_line_to_string(user_input, NovaOpcao),
+    processarSubOpcaoLoop(Jogo, NovaOpcao).
